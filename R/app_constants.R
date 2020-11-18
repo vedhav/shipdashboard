923d7ad7f8aad3ffcc072974a70f62e5f2e28c1c
vedha.viyash <vedha.viyash@zoomrx.com>
2020-11-18 21:07:19 +0530

    Remove comments

--- R/app_constants.R
+++ R/app_constants.R
@@ -5,10 +5,5 @@ default_host_ip <<- Sys.getenv("DB_HOST_IP", "127.0.0.1")
 default_host_name <<- Sys.getenv("DB_HOST_NAME", "root")
 default_host_password <<- Sys.getenv("DB_HOST_PASSWORD", "admin@123")
 
-# default_db_name <<- Sys.getenv("MYSQL_DB_NAME", "ships")
-# default_host_ip <<- Sys.getenv("DB_HOST_IP", "104.197.126.18")
-# default_host_name <<- Sys.getenv("DB_HOST_NAME", "vedha")
-# default_host_password <<- Sys.getenv("DB_HOST_PASSWORD", "MySQLP@ssW0rD")
-
 # app related global constants
 ship_types_enum <<- c('Cargo', 'Fishing', 'High Special', 'Navigation', 'Passenger', 'Pleasure', 'Tanker', 'Tug', 'Unspecified')
